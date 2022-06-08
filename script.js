fe37b9e70c825ee423616420da3f75a6a8572dd5
Lazar Narcis <lnarcis310@gmail.com>
2022-06-08 16:44:56 +0300

    new updates

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let notesList = localStorage.getItem("notes") || [];
 
 if (notesList.length != 0) notesList = JSON.parse(notesList);
 
+function showPart (string) {
+    let maxChars = 100;
+    let text = string.substring(0, maxChars) + "...";
+    return text;
+}
+
 function addNote() {
     let titleNote = document.querySelector("#titleNote");
     let textNote = document.querySelector("#textNote");
@@ -39,10 +45,18 @@ function showNotes() {
         titleHTML.className = "titleHTML";
         let textHTML = document.createElement("p");
         textHTML.className = "textHTML";
+        let showMore = document.createElement("p");
+        showMore.id = "showMore";
+        showMore.innerText = "Show More...";
         titleHTML.innerHTML = Number(notesList.indexOf(notesList[i]) + 1) + ". " + notesList[i].title;
-        textHTML.innerHTML = notesList[i].text;
+        textHTML.innerHTML = notesList[i].text.length > 150 ? showPart(notesList[i].text) : notesList[i].text;
         noteHTML.appendChild(titleHTML);
         noteHTML.appendChild(textHTML);
+        showMore.addEventListener("click", () => {
+            textHTML.innerHTML = notesList[i].text;
+            noteHTML.removeChild(showMore);
+        });
+        if (notesList[i].text.length > 150) noteHTML.appendChild(showMore);
         notes.appendChild(noteHTML);
     }
 }
